56895a93095286bff3434b158182b4dbfd1b17f1
gwangyong <key4168@icloud.com>
2021-12-18 19:17:44 +0900

    feat: 회원가입 기능 구현(REST API는 아님) 회원가입 버튼을 누르면 Alert이 나오도록 하였음 각 항목들 중에 위에부터 입력이 안된 것들은 그에 대한 Alert 이름, 아이디, 비밀번호, 비밀번호 체크 확인이 다 끝나면 완료 Aler이 나옴 완료 Alert의 "확인"을 누르면, RootViewController로 이동

--- iOS RestApi App Project/SignUpViewController.swift
+++ iOS RestApi App Project/SignUpViewController.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    
+    var essentialFieldList = [UITextField]()
+    
     @IBOutlet weak var signUpNameTextField: UITextField!
     @IBOutlet weak var signUpIdTextField: UITextField!
     @IBOutlet weak var signUpPasswordTextField: UITextField!
@@ -18,9 +20,86 @@ class SignUpViewController: UIViewController {
             $0?.layer.borderColor = UIColor.black.cgColor
             $0?.layer.cornerRadius = 10
         }
-        
+        essentialFieldList = [signUpNameTextField, signUpIdTextField, signUpPasswordTextField, signUpVerifyPasswordTextField]
     }
-
+    
     @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        
+        // 위에 viewDidLoad() 에서 정의해준 essentialFieldList, 필수 정의 항목에 있는 것들 다 있는지 확인하면서 각 함수들로 확인, 에러 Alert
+        for field in essentialFieldList {
+            if !isFilled(field) {
+                signUpAlert(field)
+                break
+            }
+        }
+        
+        func isFilled(_ textField: UITextField) -> Bool {
+            guard let text = textField.text, !text.isEmpty else {
+                return false
+            }
+            return true
+        }
+        
+        // 비어있는 곳이 있을때 나오는 Alert 종류들
+        func signUpAlert(_ field: UITextField) {
+            DispatchQueue.main.async {
+                var title = ""
+                switch field {
+                case self.signUpNameTextField:
+                    title = "이름을 입력해주세요."
+                case self.signUpIdTextField:
+                    title = "아이디를 입력해주세요"
+                case self.signUpPasswordTextField:
+                    title = "비밀번호를 입력해주세요."
+                case self.signUpVerifyPasswordTextField:
+                    title = "비밀번호를 확인해주세요."
+                default:
+                    title = "Error"
+                }
+                let controller = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "확인", style: .cancel) { (action) in
+                    
+                }
+                
+                controller.addAction(okAction)
+                self.present(controller, animated: true, completion: nil)
+            }
+        }
+        
+        // 비밀번호가 일치하는지 passwordAlert 함수로 이동해서 확인
+        guard let password = signUpPasswordTextField.text, let passwordCheck = signUpVerifyPasswordTextField.text, password == passwordCheck else {
+            passwordAlert(title: "비밀번호가 일치하지 않습니다.")
+            return
+        }
+        
+        showInfoAlert(with: "회원가입이 완료되었습니다.")
+        
+        // 위에 코드들의 오류 Alert이 아닌, 전부 통과된 확인 Alert
+        func showInfoAlert(with title: String) {
+            DispatchQueue.main.async {
+                let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+                
+                let okAction = UIAlertAction(title: "확인", style: .default) { (action) in
+                    // 회원가입이 되고 확인을 누르고 난 후, completion handler를 사용해 RootViewController로 이동
+                    self.navigationController?.popToRootViewController(animated: true)
+                }
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        
+        
+        
+        func passwordAlert(title: String) {
+            DispatchQueue.main.async {
+                let controller = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+                let okAlert = UIAlertAction(title: "확인", style: .cancel) { (action) in
+                    
+                }
+                
+                controller.addAction(okAlert)
+                self.present(controller, animated: true, completion: nil)
+            }
+        }
     }
 }
